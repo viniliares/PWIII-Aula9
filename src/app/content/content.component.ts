ffeae14c5e758d6a048c558f5ce5579281af129c
Vinicius <vinicius.soares71@etec.sp.gov.br>
2019-09-20 22:27:45 -0300

    Altertando o Bootstrap

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typed from 'typed.js';
+
 @Component({
   selector: 'has-content',
   templateUrl: './content.component.html',
@@ -10,6 +12,20 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['ETEC - Banco Digital', 'Peça a portabilidade e venha ser ETEC hoje mesmo', 'Inovavor, digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+	
+	const typed = new Typed('.typing-element', options);
   }
 
 }
